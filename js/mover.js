f0e926527d709dd0f387347c25a190b5e3402cab
Oscar Castañeda <62982702+oscar-castanedar@users.noreply.github.com>
2020-11-08 11:48:38 -0600

    Add files via upload

---
+++ js/mover.js
@@ -0,0 +1,15 @@
+var x=document.getElementById("login");
+var y=document.getElementById("registrar");
+var z=document.getElementById("elegir");
+
+function login(){
+	x.style.left="50px";
+	y.style.left="450px";
+	z.style.left="0px";
+}
+
+function registrar(){
+	x.style.left="-400px";
+	y.style.left="50px";
+	z.style.left="120px";
+}
